8a961ccff2dcd025edab9faca3ed5f4b8155e858
Nitesh <niteshsaini1296@gmail.com>
2025-01-08 21:30:54 +0530

    Code Added

--- Invoice/src/main/java/com/payvang/Invoice/Services/InvoiceService.java
+++ Invoice/src/main/java/com/payvang/Invoice/Services/InvoiceService.java
@@ -1,23 +1,32 @@
 package com.payvang.Invoice.Services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.payvang.Invoice.Entities.Invoice;
 import com.payvang.Invoice.Repositories.InvoiceRepository;
 
 @Service
 public class InvoiceService {
-	
-	@Autowired
-	private InvoiceRepository invoiceRepository;
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
+    public Invoice saveInvoice(Invoice invoice) {
+        return invoiceRepository.save(invoice);
+    }
+
+    public List<Invoice> saveInvoices(List<Invoice> invoices) {
+        return invoiceRepository.saveAll(invoices);
+    }
+
+    public List<Invoice> getAllInvoices() {
+        return invoiceRepository.findAll();
+    }
+
+    public Invoice getInvoiceById(Long id) {
+        return invoiceRepository.findById(id).orElse(null);
+    }
 }
